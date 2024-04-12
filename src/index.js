adc80117d2e5e841f96b383df011331a87c3afb4
onlinearivu <garumugamtvm@gmail.com>
2024-04-12 09:58:27 +0530

    Path Param

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/robofriends' element={<App />} />
+      <Route path='/robofriends/' element={<App />} />
       <Route path='/robofriends/:name' element={<Welcome />} />
     </Routes>
   </BrowserRouter>
